a45b50e650c1814cf0fe21b488fda2ae86966772
MartinRVidal <martinrodrigovidal@gmail.com>
2022-08-31 19:33:01 -0300

    tercer desafio

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import "./App.css"
 function App() {
   return (
     <header>
-      <NavBar/>
-      <ItemListContainer/>
+      <NavBar />
+      <ItemListContainer />
     </header>
   )
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import banner from '../images/banner.png'
-import "../components/ItemListContainer.css"
+import React from 'react';
+import banner from '../images/banner.png';
+import "../components/ItemListContainer.css";
+import ItemCount from '../components/ItemCount';
+
 
 const ItemListContainer = () => {
+
+    const onAdd = (cantidad) => {
+        console.log(`Compraste ${cantidad} unidades`); 
+    }
+
+
 return (
+    <div>
     <div className='div-banner'>
         <img src={banner} alt="" className='img-banner'/>
+    </div>
+    <ItemCount initial={1} stock={7} onAdd={onAdd} />
     </div>
 )
 }
